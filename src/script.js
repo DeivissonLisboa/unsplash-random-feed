ab7db4180ab8d0dd68bca738ba933095dad92aa0
Deivisson Lisboa <dvilaflor@gmail.com>
2023-02-20 18:11:55 -0300

    Create getImages function

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const CONTAINER = document.querySelector(".container")
+const API_URL = "https://source.unsplash.com/random"
+const SIZES = ["100X100", "100X200", "200X100", "100X300", "300X100"]
+
+function getRandomSize() {
+  return SIZES[Math.floor(Math.random() * SIZES.length)]
+}
+
+async function getImages(number_of_images) {
+  for (let i = 0; i < number_of_images; i++) {
+    let imgEl = document.createElement("img")
+    imgEl.alt = ""
+
+    let size = getRandomSize()
+
+    imgEl.src = `${API_URL}/${size}/?&${i}`
+
+    CONTAINER.append(imgEl)
+  }
+}
+
+getImages(10)
